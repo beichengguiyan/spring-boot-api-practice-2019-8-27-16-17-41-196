a3b9e9c65981f3294c8071927a49d8b4f21dbc53
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-28 22:58:41 +0800

    work

--- src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -1,6 +1,5 @@
 package com.tw.apistackbase.controller;
 
-import java.security.Provider.Service;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,58 +20,66 @@ import com.tw.apistackbase.model.Employee;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeResource {
-	
+
 	private static List<Employee> employees = new ArrayList<Employee>() {
 		{
-			add(new Employee(4, "alibaba1", 20, "male",6000));
-			add(new Employee(11, "tengxun2", 19, "female",7000));
-			add(new Employee(6, "alibaba3", 19, "male",8000));
+			add(new Employee(4, "alibaba1", 20, "male", 6000));
+			add(new Employee(11, "tengxun2", 19, "female", 7000));
+			add(new Employee(6, "alibaba3", 19, "male", 8000));
+			add(new Employee());
+			add(new Employee());
 		}
 	};
 
-//	@GetMapping()
-//	public ResponseEntity<List<Employee>> getAll() {
-//		return ResponseEntity.ok(employees);
-//	}
-//
-//	@GetMapping(path = "/{employeeId}")
-//	public ResponseEntity<Employee> getEmployee(@PathVariable int employeeId) {
-//		return ResponseEntity.ok(employees.get(employeeId));
-//	}
-//
-//	@GetMapping(path = "/name")
-//	public ResponseEntity<List<Employee>> getEmployeeByName(
-//			@RequestParam(name = "name", required = false) String name) {
-//		List<Employee> employeesResult = new ArrayList<Employee>();
-//		for (Employee employee : employees) {
-//			if (employee.getName().contains(name)) {
-//				employeesResult.add(employee);
-//			}
-//		}
-//		return ResponseEntity.ok(employeesResult);
-//	}
-//
-//	@PostMapping(consumes = "application/json")
-//	public ResponseEntity<List<Employee>> addEmployee(@RequestBody Employee employee) {
-//		employees.add(employee);
-//		return ResponseEntity.status(HttpStatus.CREATED).build();
-//	}
-//
-//	@PutMapping(consumes = "application/json")
-//	public ResponseEntity<List<Employee>> updateEmployee(@RequestBody Employee employee) {
-//		for (int i = 0; i < employees.size(); i++) {
-//			if (employees.get(i).getId() == employee.getId()) {
-//				employees.set(i, employee);
-//			}
-//		}
-//		return ResponseEntity.status(HttpStatus.CREATED).build();
-//	}
-//
-//	@DeleteMapping(path = "/{employeeId}")
-//	public ResponseEntity<List<Employee>> deldeteEmployee(@PathVariable int employeeId) {
-//				employees.remove(employeeId);
-//		return ResponseEntity.status(HttpStatus.CREATED).build();
-//	}
-	
-}
+	@GetMapping("/")
+	public ResponseEntity<List<Employee>> getAll() {
+		return ResponseEntity.ok(employees);
+	}
 
+	@GetMapping(path = "/{employeeId}")
+	public ResponseEntity<Employee> getEmployee(@PathVariable int employeeId) {
+		return ResponseEntity.ok(employees.get(employeeId));
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<Employee>> getEmployeeByName(@RequestParam(name = "page", required = false) int page,
+			@RequestParam(name = "pageSize", required = false) int pageSize){
+			List<Employee> employeesResult = new ArrayList<Employee>();
+			if (page == 1) {
+				for (int i = 0; i < pageSize; i++) {
+					employeesResult.add(employees.get(i));
+				}
+			}
+			return ResponseEntity.ok(employeesResult);
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<Employee>> getEmployeeByName(@RequestParam(name = "gender", required = false) String gender){
+			List<Employee> employeesResult = new ArrayList<Employee>();
+			for (int i = 0; i < employees.size(); i++) {
+				if (employees.get(i).getGender().equals(gender)) {
+					employeesResult.add(employees.get(i));
+				}
+			}
+			return ResponseEntity.ok(employeesResult);
+	}
+
+	@PostMapping(consumes = "application/json")
+	public ResponseEntity<List<Employee>> addEmployee(@RequestBody Employee employee) {
+		employees.add(employee);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@PutMapping(path = "/{employeeId}")
+	public ResponseEntity<List<Employee>> updateEmployee(@PathVariable int employeeId,@RequestBody Employee employee) {
+		employees.set(employeeId, employee);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@DeleteMapping(path = "/{employeeId}")
+	public ResponseEntity<List<Employee>> deldeteEmployee(@PathVariable int employeeId) {
+				employees.remove(employeeId);
+		return ResponseEntity.ok(employees);
+	}
+
+}
